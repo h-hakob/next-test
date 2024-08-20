50c676338f0b42ee5e4e99c0260214022c518edb
Hakob <hakhakobyanas@gmail.com>
2024-08-20 08:13:02 +0400

    fix for docker

--- docker-bootstrap-app.sh
+++
@@ -1,3 +0,0 @@
-DATABASE_URL="postgres://postgres:postgres@db:5432/appdb?sslmode=disable" npx prisma migrate deploy
-# start app
-DATABASE_URL="postgres://postgres:postgres@db:5432/workler?sslmode=disable" node server.js
