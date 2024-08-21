7992f135655c047b296eda6d7d7183491a5e5b57
Hakob <hakhakobyanas@gmail.com>
2024-08-21 16:19:07 +0400

    fix

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -2,7 +2,7 @@ import { auth } from "auth"
 import { prisma } from "@/lib/prisma";
 
 export const GET = auth(async (req) => {
-  if (req.auth) {
+  // if (req.auth) {
     const limit = 4;
     let skip = (parseInt(req.nextUrl.searchParams.get("page") || "1") - 1) * limit;
 
@@ -18,7 +18,7 @@ export const GET = auth(async (req) => {
       count_of_pages: Math.ceil(count / limit),
       transactions
     }})
-  }
+  // }
 
-  return Response.json({ message: "Not authenticated" }, { status: 401 })
+  // return Response.json({ message: "Not authenticated" }, { status: 401 })
 })
