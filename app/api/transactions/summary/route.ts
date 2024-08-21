a18fdbb63e1154f98399ac5208031022665b474e
Hakob <hakhakobyanas@gmail.com>
2024-08-21 16:21:59 +0400

    fix

--- app/api/transactions/summary/route.ts
+++ app/api/transactions/summary/route.ts
@@ -2,7 +2,7 @@ import { auth } from "auth"
 import { prisma } from "@/lib/prisma";
 
 export const GET = auth(async (req) => {
-    // if (req.auth) {
+    if (req.auth) {
       const data = await prisma.$queryRaw<any[]>`select cast(count(amount) as INTEGER) as active_items, sum(amount) total_amount from transactions t where status is true`
 
       if (!data || !data[0]) {
@@ -16,5 +16,5 @@ export const GET = auth(async (req) => {
       return Response.json({ data: data[0] })
     // }
   
-    // return Response.json({ message: "Not authenticated" }, { status: 401 })
+    return Response.json({ message: "Not authenticated" }, { status: 401 })
   })
